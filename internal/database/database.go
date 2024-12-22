81f24906f7919f53c779733f62de828cb77e3716
Akmal Sindarov <asauz777@gmail.com>
2024-12-22 10:26:46 +1000

    Update database.go

--- internal/database/database.go
+++ internal/database/database.go
@@ -19,7 +19,8 @@ var DB Distance
 
 func ConnectDb() {
 	dsn := fmt.Sprintf(
-		"host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai",
+		"host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai",
+		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
